b0fc6cb90d58dc2dec1bd46eb6e58900ce40ed38
wym <wym15935107902@163.com>
2025-11-03 16:24:01 +0800

    多余的main.tsx文件

--- 003inversifyReact/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { init, diConfig } from "./fronted/index";
-import type { IContainer } from "@/types";
-
-document.addEventListener("DOMContentLoaded", () => {
-  init((container: IContainer) => {
-    diConfig(container);
-  });
-});
